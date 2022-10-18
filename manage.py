7e69cf24855d8b9a829a82e54302b22d0583edb7
rv <rv.monnier@free.fr>
2022-10-18 16:43:06 +0200

    test and lint in config.yml

--- manage.py
+++ manage.py
@@ -1,11 +1,6 @@
 import os
 import sys
 
-# Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie quam lobortis leo consectetur ullamcorper non id est. Praesent dictum, nulla eget feugiat sagittis, sem mi convallis eros,
-# vitae dapibus nisi lorem dapibus sem. Maecenas pharetra purus ipsum, eget consequat ipsum lobortis quis. Phasellus eleifend ex auctor venenatis tempus.
-# Aliquam vitae erat ac orci placerat luctus. Nullam elementum urna nisi, pellentesque iaculis enim cursus in. Praesent volutpat porttitor magna, non finibus neque cursus id.
-# ttttttttttttttttttttttttttttttttttttttttttttttttttttt ttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt
-
 
 def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oc_lettings_site.settings")
