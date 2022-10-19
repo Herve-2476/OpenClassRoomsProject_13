26a1f2fdafd2d3115d354019dd04d374844d4233
rv <rv.monnier@free.fr>
2022-10-19 11:24:03 +0200

    heroku

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = "fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s"
 # SECRET_KEY = "#__0n)2r5b-h46ik+t(605(xvs6uf-a@ur(xq0+-=z%=ouu(o*"
 # SECRET_KEY = os.environ.get("SECRET_KEY", SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", "localhost"]
 
